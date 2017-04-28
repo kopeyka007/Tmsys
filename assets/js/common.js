f4424e1914e978e43bc6fb86cac810c7066ccb61
Valeria <vt@div-art.com>
2017-04-28 18:04:57 +0300

    create new print and trapeze

--- assets/js/common.js
+++ assets/js/common.js
@@ -67,14 +67,15 @@
 (function() {
 	angular.module("app").controller("AppCtrl", function($scope, print) {
 		$scope.laying = 'evenly';
-		$scope.board = {'x': 14, 'y': [160, 90]};
+		$scope.board = {'x': 14, 'y': [150, 90]};
 		$scope.seam = 1;
 		$scope.split = 80;
 
 		$scope.type = '0';
-		$scope.terrace = {'x': [15, 10], 'y': [1.6, 2]};
-		$scope.margin = {'x': [0, 0], 'y': [0, 0]};
-		$scope.layout = '1';
+		$scope.terrace = {'x': [0.75, 0.75], 'y': [1.5, 1.5] , 'z':[0.75, 0.75]};
+		$scope.margin = {'x': [0, 0], 'y': [0, 0], 'z':[0, 0]};
+		$scope.layout = '0';
+		$scope.angle = '0';
 
 		$scope.t = false;
 		$scope.b = [{}, {}];
@@ -94,78 +95,181 @@
 		$scope.calculate = function() {
 			$scope.restsStack = [];
 			$scope.boardsCount = [0, 0];
-			if($scope.layout == '0')
-			{
-				$scope.t = {'x': $scope.terrace.x[0] * 100, 'y': $scope.terrace.y[0] * 100};
-			}
-			else
-			{
-				$scope.t = {'x': $scope.terrace.y[0] * 100, 'y': $scope.terrace.x[0] * 100};
-			}
 			
 			$scope.b[0] = {'x': ($scope.board.x * 1 + $scope.seam * 1), 'y': $scope.board.y[0] * 1};
 			$scope.b[1] = {'x': ($scope.board.x * 1 + $scope.seam * 1), 'y': $scope.board.y[1] * 1};
 			$scope.startY = ($scope.twoBoards ? $scope.b[1].y : $scope.split) * 1;
 
+			print.reset();
+
 			if ($scope.type == '0')
 			{
-				console.log('Terrace type 1');
-				$scope.compute();
+				$scope.computeType0();
 			}
-			else if($scope.type == '1' || $scope.type == '2')
+			else if($scope.type == '1')
 			{
-				console.log('Terrace type ' + $scope.type);
-				for(var i = 0; i <= 1; i++)
-				{
-					$scope.circle = ($scope.type == '2') && (i == 1) ? true : false;
-
-					if($scope.layout == '0')
-					{
-						$scope.t = {'x': $scope.terrace.x[i] * 100, 'y': $scope.terrace.y[i] * 100};
-					}
-					else
-					{
-						$scope.t = {'x': $scope.terrace.y[i] * 100, 'y': $scope.terrace.x[i] * 100};
-					}
-					
-					$scope.compute();						
-				}
+				$scope.computeType1();
 			}
+			else if ($scope.type == '2')
+			{
+				$scope.computeType2();
+			}
+			else if ($scope.type == '3')
+			{
+				$scope.computeType3();
+			}
+
+			print.render();
+		};
+
+		$scope.computeType0 = function() {
+			$scope.t = $scope.getT(0);
+			$scope.rectangle();
+		};
+
+		$scope.computeType1 = function() {
+			for (var i = 0; i <= 1; i++)
+			{
+				$scope.t = $scope.getT(i);
+				$scope.rectangle();
+			}
+		};
+
+		$scope.computeType2 = function() {
+			$scope.t = $scope.getT(0);
+			$scope.rectangle();
+
+			$scope.t = $scope.getT(1);
+			$scope.circle();
+		};
+
+		$scope.computeType3 = function() {
+			for (var i = 0; i <= 1; i++)
+			{
+				if ($scope.layout == 0)
+				{
+					$scope.t = {'x': $scope.terrace.x[i] * 100, 'y': $scope.terrace.y[i] * 100, 'z': $scope.terrace.z[i] * 100};
+					print.init($scope.t.x, $scope.t.y, $scope.type, $scope.angle);
+					$scope.trapeze();
+				}
+				else
+				{
+					$scope.t = {'x': $scope.terrace.z[i] * 100, 'y': $scope.terrace.y[i] * 100};
+					print.init($scope.t.x, $scope.t.y, $scope.type, $scope.angle);
+					$scope.rectangle();
+
+					$scope.t = {'x': $scope.terrace.x[i] * 100, 'y': ($scope.terrace.y[i] - $scope.terrace.z[i]) * 100};
+					print.init($scope.t.x, $scope.t.y, $scope.type, $scope.angle);
+					$scope.triangle();
+				}	 
+			}
+		};
+
+		$scope.trapeze = function() {
+			$scope.clearVars();
+
+			var cols = $scope.colsCount();
+			for (var i = 0; i < cols; i++)
+			{
+				$scope.maxColY = $scope.getMaxTrapezeColY(i);
+				$scope.printStep(i);
+				$scope.fillCol();
+			}	
+		};
+
+		$scope.getT = function(i) {
+			var t = {};
+			if ($scope.layout == '0')
+			{
+				t = {'x': $scope.terrace.x[i] * 100, 'y': $scope.terrace.y[i] * 100};
+				print.init(t.x, t.y, $scope.type, $scope.angle);
+			}
+			else
+			{
+				t = {'x': $scope.terrace.y[i] * 100, 'y': $scope.terrace.x[i] * 100};
+				print.init(t.y, t.x, $scope.type, $scope.angle);
+			}
+			return t;
+		};
+
+		$scope.rectangle = function() {
+			$scope.clearVars();
+
+			$scope.maxColY =  $scope.t.y;
+			var cols = $scope.colsCount();
+			for (var i = 0; i < cols; i++)
+			{
+				$scope.printStep(i);
+				$scope.fillCol();
+			}	
+		};
+
+		$scope.circle = function() {
+			$scope.clearVars();
+			var cols = $scope.colsCount();
+			if ($scope.layout == 0)
+			{
+				cols = $scope.middleCol(cols);
+				for (var i = (cols / 2) - 1; i >= 0; i--)
+				{
+					$scope.maxColY = $scope.getMaxCircleColY(i);
+					$scope.printStep(i);
+					$scope.fillCol();
+
+					$scope.printStep(cols - i - $scope.mirrorStart);
+					$scope.fillCol();
+				}	
+			}
+			else
+			{	
+				for (var i = 0; i < cols; i++)
+				{
+					$scope.maxColY = $scope.getMaxCircleHorizontal(i);
+					$scope.printStep(i);
+					$scope.fillCol();
+				}	
+			}
+			
 		};
 
 		$scope.deltaFromBegin = 0;
-		$scope.pairCounter = 1;
-		$scope.colsCount = function() {
-			var cols =  Math.ceil($scope.t.x / $scope.b[$scope.boardType].x);
-			if (cols % 2 > 0)
-			{
-				$scope.pairCounter = 0;
-				$scope.deltaFromBegin = $scope.b[$scope.boardType].x / 2;
-				cols--;
-
-				print.row(cols / 2);
-				$scope.fillCol(cols / 2);
-			}
-			return cols / 2;
+		$scope.mirrorStart = 1;
+		$scope.paired = function(cols){
+			return ! (cols % 2 > 0)
 		};
-		
-		$scope.compute = function()
-		{
+
+		$scope.middleCol = function(cols) {
+			var pair = $scope.paired(cols);
+			if ( ! pair)
+			{
+				$scope.mirrorStart = 0;
+				$scope.deltaFromBegin = $scope.b[$scope.boardType].x / 2;
+				cols--
+				$scope.maxColY =  $scope.t.y;
+				$scope.printStep(cols / 2);
+				$scope.fillCol();
+			}
+			return cols;
+		};
+
+		$scope.triangle = function() {
 			$scope.clearVars();
 
-			print.init($scope.t.x, $scope.t.y, $scope.b[0].x);
-
-			var halfCols = $scope.colsCount();
-			for (var i = (halfCols - 1); i >= 0; i--)
+			var cols = $scope.colsCount();
+			
+			for (var i = 0; i < cols; i++)
 			{
-				print.row(i);
-				$scope.fillCol(i);
-				print.row(2 * halfCols - i - $scope.pairCounter);
-				$scope.fillCol(i);
-			}
+				$scope.maxColY = $scope.getMaxTriangleColY(i);
+				$scope.printStep(i);
+				$scope.fillCol();
+			}	
 		};
 
-		$scope.fillCol = function(colNumber) {
+		$scope.colsCount = function() {
+			return Math.ceil($scope.t.x / $scope.b[$scope.boardType].x);
+		};
+
+		$scope.fillCol = function() {
 			$scope.boardType = 0;
 			var start = $scope.boardY();
 			if ($scope.laying == 'emporally' || $scope.twoBoards)
@@ -181,33 +285,22 @@
 				}
 			}
 
-			var maxColY =  $scope.t.y;
-			if ($scope.circle)
+			var colY = $scope.nextBoard(start);
+			while (colY < $scope.maxColY)
 			{
-				maxColY = $scope.getMaxcolY(colNumber);
+				colY += $scope.nextBoard($scope.boardY(), colY);
 			}
-
-			var colY = $scope.nextBoard(start, maxColY);
-			while (colY < maxColY)
-			{
-				colY += $scope.nextBoard($scope.boardY(), maxColY, colY);
-			}
-			$scope.addRest(colY - maxColY);
+			$scope.addRest(colY - $scope.maxColY);
 		};
 
-		$scope.nextBoard = function(board, maxColY, colY) {
+		$scope.nextBoard = function(board, colY) {
 			colY = colY || 0;
-			var left = (maxColY - colY);
+			var left = ($scope.maxColY - colY);
 			var part = left > board ? board : left;
 
 			if ( ! $scope.checkInStack(part))
 			{
-				/*if ($scope.twoBoards)
-				{
-					$scope.boardType = $scope.betterBoard(part);
-				}*/
-
-				print.board(part, $scope.boardType);
+				$scope.printBoard(part, $scope.boardType);
 				$scope.boardsCount[$scope.boardType]++;
 				$scope.addRest($scope.boardY() - part);
 			}
@@ -220,30 +313,6 @@
 			return part;
 		};
 
-		$scope.betterBoard = function(part) {
-			var delta = [0, 0];
-			delta[0] = $scope.b[0].y - part;
-			delta[1] = $scope.b[1].y - part;
-
-			if (delta[0] >= 0 && delta[1] >= 0)
-			{
-				if (delta[0] > delta[1])
-				{
-					return 1;
-				}
-				else if (delta[0] < delta[1])
-				{
-					return 0;
-				}
-				else
-				{
-					return ($scope.b[0].y <= $scope.b[1].y ? 0 : 1);
-				}
-			}
-
-			return $scope.boardType;
-		};
-
 		$scope.checkInStack = function(part) {
 			$scope.restsStack.sort(function(a, b) { return a > b ? 1 : (a < b ? -1 : 0); });
 			for (var k in $scope.restsStack)
@@ -252,7 +321,7 @@
 				{
 					if ($scope.restsStack[k] >= part)
 					{
-						print.board(part, 2);
+						$scope.printBoard(part, 2);
 						$scope.restsStack[k] -= part;
 						return true;
 					}
@@ -281,7 +350,7 @@
 			$scope.restsStack = newStack;
 		};
 
-		$scope.getMaxcolY = function(colNumber) {
+		$scope.getMaxCircleColY = function(colNumber) {
 			var r = $scope.getRadius();
 			var partR = $scope.t.x / 2;
 			var xFromBegin = (colNumber + 1) * $scope.b[$scope.boardType].x + $scope.deltaFromBegin;
@@ -295,10 +364,57 @@
 			return $scope.t.y - delta;
 		};
 
-		$scope.getRadius = function() {
-			return ($scope.t.y * $scope.t.y  + $scope.t.x * $scope.t.x / 4 ) / 2 / $scope.t.y;
+		$scope.getMaxCircleHorizontal = function(colNumber) {
+			var r = $scope.getRadius(true);
+			var x1 = $scope.t.x - (colNumber * $scope.b[$scope.boardType].x);
+			return Math.ceil(Math.sqrt( (8 * x1 * r) - (4 * x1 * x1)));
 		};
 
+		$scope.getMaxTrapezeColY = function(colNumber) {
+			var a = $scope.t.y - $scope.t.z;
+			var ctgA = a / $scope.t.x;
+			var c = $scope.t.x - (colNumber + 1) * $scope.b[$scope.boardType].x ;
+			return Math.ceil($scope.t.y - c * ctgA);
+		};
+
+		$scope.getMaxTriangleColY = function(colNumber) {
+			var b = (colNumber + 1) * $scope.b[$scope.boardType].x ;
+			return   Math.ceil(($scope.t.y / $scope.t.x) * b);
+		};
+
+		$scope.getRadius = function(hor) {
+			hor = hor || false;
+			if( ! hor)
+			{
+				return ($scope.t.y * $scope.t.y  + $scope.t.x * $scope.t.x / 4 ) / 2 / $scope.t.y;
+			}
+			else
+			{
+				return ($scope.t.x * $scope.t.x  + $scope.t.y * $scope.t.y / 4 ) / 2 / $scope.t.x;
+			}
+		};
+
+		$scope.printBoard = function(part, type) {
+			if ($scope.layout == '0')
+			{
+				print.board($scope.b[$scope.boardType].x, part, type);
+			}
+			else
+			{
+				print.board(part, $scope.b[$scope.boardType].x, type);
+			}
+		};
+
+		$scope.printStep = function(i) {
+			if ($scope.layout == '0')
+			{
+				print.col(i);
+			}
+			else
+			{
+				print.row(i);
+			}
+		}
 	});
 })();
 
--- assets/js/print.js
+++ assets/js/print.js
@@ -1,63 +1,28 @@
 (function() {
 	angular.module("app").factory("print", function() {
 		var factory = {};
-		factory.render = true;
-		factory.canvases = [];
-		factory.current = '';
-		factory.k = 1;
-		factory.place = 'top';
+		factory.current = 0;
+		factory.data = [];
+		factory.col_number = 0;
+		factory.row_number = 0;
+		factory.direction = 'col';
 
-		factory.init = function(width, height, col) {
-			this.current = 'canvas_' + this.canvases.length;
+		factory.init = function(width, height, type, angle) {
+			var canvas = {};
+			canvas.width = width;
+			canvas.height = height;
+			canvas.type = type;
+			canvas.angle = angle;
 
-			this.k = 1;
-			this.scale(width, height);
-			this.col = col;
-			var style = {'position': 'fixed',
-						 'z-index': '1000',
-						 'bottom': this.bottom() + 'px',
-						 'left': this.left() + 'px',
-						 'border': 'solid #333 1px',
-						 'background': 'rgba(255, 255, 255, 0.5)',
-						 'width': (width / this.k) + 'px',
-						 'height': (height / this.k) + 'px'};
-			var canvas = '<div id="' + this.current + '" class="canvas" style="' + this.style(style) + '"></div>';
-			if (this.render)
-			{
-				$('body').append(canvas);
-				this.canvases.push({'width': (width / this.k), 'height': (height / this.k), 'id': this.current});
-				$('.canvas').on('click', this.reset);
-			}
+			this.current = this.data.length;
+			this.data.push({'canvas': canvas,
+					   		'boards': []});
 		};
 
 		factory.reset = function() {
-			$('.canvas').remove();
-			factory.canvases = [];
-			factory.current = '';
-		};
-
-		factory.bottom = function() {
-			var bottom = 20;
-			if (this.place == 'top')
-			{
-				for (var k in this.canvases)
-				{
-					bottom += (this.canvases[k].height + 20)
-				}
-			}
-			return bottom;
-		};
-
-		factory.left = function() {
-			var left = 20;
-			if (this.place == 'right')
-			{
-				for (var k in this.canvases)
-				{
-					left += (this.canvases[k].width + 20)
-				}
-			}
-			return left;
+			this.col_number = 0;
+			this.data = [];
+			$('.canvas').html('');
 		};
 
 		factory.style = function(rules) {
@@ -69,12 +34,19 @@
 			return result;
 		};
 
-		factory.row = function(row) {
-			this.row_number = row;
+		factory.col = function(col) {
+			this.direction = 'col';
+			this.col_number = col;
 			this.y = 0;
 		};
 
-		factory.board = function(y, type) {
+		factory.row = function(row) {
+			this.direction = 'row';
+			this.row_number = row;
+			this.x = 0;
+		};
+
+		factory.board = function(width, height, type) {
 			type = type || 0;
 			var color = '0, 0, 0';
 			switch (type)
@@ -83,36 +55,182 @@
 				case 1: color = '0, 0, 255'; break;
 				case 2: color = '255, 0, 0'; break;
 			}
-			var style = {'position': 'absolute',
-						 'z-index': '0',
-						 'bottom': (this.y / this.k) + 'px',
-						 'left': ((this.row_number * this.col) / this.k) + 'px',
-						 'border': 'solid #333 1px',
-						 'background': 'rgba(' + color + ', 0.5)',
-						 'width': (this.col / this.k) + 'px',
-						 'height': (y / this.k) + 'px'};
-			var board = '<div style="' + this.style(style) + '"></div>';
-			if (this.render)
-			{
-				$('#' + this.current).append(board);
-			}
 
-			this.y += y;
+			var cols = this.getColsCount(width);
+			var rows = this.getColsRow(width);
+			var board = {};
+			board.type = type;
+			board.color = color;
+			board.width = width;
+			board.height = height;
+			if (this.direction == 'col')
+			{
+				board.x = this.col_number * width;
+				board.y = this.y;
+				this.y += height;
+			}
+			else
+			{
+				board.x = this.x;
+				board.y = this.row_number * height;
+				this.x += width;
+			}
+			this.data[this.current].boards.push(board);
 		};
 
 		factory.scale = function(x, y) {
-			var window_x = $(window).outerWidth() - 40;
+			var k = 1;
+			var window_x = $('.canvas').outerWidth() - 40;
 			if (x > window_x)
 			{
-				this.k = x / window_x;
-				this.place = 'top';
+				k = x / window_x;
 			}
 
-			var window_y = $(window).outerHeight() - 40;
+			var window_y = $('.canvas').outerHeight() - 40;
 			if (y > window_y)
 			{
-				this.k = y / window_y;
-				this.place = 'right';
+				k = y / window_y;
+			}
+
+			return k;
+		};
+
+		factory.style = function(list) {
+			var arr = [];
+			for (var i in list)
+			{ 
+			    arr.push(i + ': ' + list[i] + ';');
+			}
+			return list = arr.join(' ');
+		};
+
+		factory.drawCanvas = function() {
+			var width = 0;
+			var height = 0;
+
+			for (var key in this.data)
+			{
+				var type = this.data[key].canvas.type;
+				var	angle = this.data[key].canvas.angle;
+				if (type <=2 )
+				{
+					if (angle == '0' || angle == '180')
+					{
+						height += this.data[key].canvas.height;
+						width = Math.max(width, this.data[key].canvas.width);
+					}
+
+					if (angle == '90' || angle == '270')
+					{
+						width += this.data[key].canvas.width;
+						height = Math.max(height, this.data[key].canvas.height);
+					}
+				}
+			}
+			var canvasWidth = $('.canvas').outerWidth();
+			var k = width / canvasWidth;
+			var canvasHeight = height / k;
+			$('.canvas').height(canvasHeight);
+			return k;
+		};
+
+		factory.render = function() {
+			var k = this.drawCanvas();
+			var offsetX = 0;
+			var offsetY = 0;
+			for (var key in this.data)
+			{
+				var c = this.data[key].canvas;
+
+				var style = {
+					'width': (c.width / k) + 'px',
+					'height':(c.height / k) + 'px',
+				};
+				var type = this.data[key].canvas.type;
+				var	angle = this.data[key].canvas.angle;
+				if (type <= 2)
+				{
+					if (angle == '0' || angle == '90')
+					{
+						if (key > 0)
+						{
+							if (angle == '0')
+							{
+								offsetX = (offsetX - (c.width / k)) / 2;
+							}
+
+							if (angle == '90')
+							{
+								offsetY = (offsetY - (c.height / k)) / 2;
+							}
+						}
+
+						style.left = offsetX + 'px';
+						style.bottom = offsetY + 'px';
+					}
+
+					if (angle == '180' || angle == '270')
+					{
+						if (key > 0)
+						{
+							if (angle == '180')
+							{
+								offsetX = (offsetX - (c.width / k)) / 2;
+							}
+
+							if (angle == '270')
+							{
+								offsetY = (offsetY - (c.height / k)) / 2;
+							}
+						}
+
+						style.right = offsetX + 'px';
+						style.top = offsetY + 'px';
+					}
+
+					if (angle == '0' || angle == '180')
+					{
+						offsetX = (c.width / k);
+						offsetY += (c.height / k);
+					}
+
+					if (angle == '90' || angle == '270')
+					{
+						offsetX += (c.width / k);
+						offsetY = (c.height / k);
+					}
+				}
+
+				var id = 'box-' + key;
+				$('.canvas').append('<div class="box" id="' + id + '" style="' + this.style(style) + '"></div>');
+				for (var i = 0; i < this.data[key].boards.length; i++)
+				{
+					var board = this.data[key].boards[i];
+					var style = {
+						'background': 'rgba(' + board.color + ', 0.5)',
+						'width': (board.width / k) + 'px',
+						'height':(board.height / k) + 'px',
+						'left': (board.x / k) + 'px',
+						'bottom': (board.y / k) + 'px'
+					};
+
+					$('#' + id).append('<div class="board" style="' + this.style(style) + '"></div>');
+				}
+			}
+		};
+
+		factory.getColsCount = function(width) {
+			return Math.ceil(this.data[this.current].canvas.width / width);
+		};
+
+		factory.getColsRow = function(width) {
+			return Math.ceil(this.data[this.current].canvas.height / width);
+		};
+		
+		factory.getPair = function(number) {
+			if(number % 2 == 0)
+			{
+				return true
 			}
 		};
 
