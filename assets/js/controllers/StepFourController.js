377da79b12aab90cef67bf5a46b27fec8c711e3b
Valeria <vt@div-art.com>
2017-07-27 14:09:08 +0300

    add json material and fix duble controllers

--- assets/js/controllers/StepFourController.js
+++ assets/js/controllers/StepFourController.js
@@ -1,15 +1,24 @@
 (function() {
-	angular.module("app").controller("StepFourController", function($rootScope, $scope, $location, $routeParams, print, connect) {
+	angular.module("app").controller("StepFourController", function($rootScope, $scope, $location, $routeParams, print,  request, connect) {
 		$scope.calculate();
 		$scope.tab = 1;
 
 	    $scope.setTab = function(newTab){
-	      $scope.tab = newTab;
+	       $scope.tab = newTab;
 	    };
 
 	    $scope.isSet = function(tabNum){
-	      return $scope.tab === tabNum;
+	       return $scope.tab === tabNum;
 	    };
+
+	    $scope.getArrayMaterials = function () { 
+			request.send('/backEnd/material.json', {}, function(data) {
+				$scope.materials  = data.data;
+			});
+		};
+
+		$scope.getArrayMaterials();
+		console.log($scope.materials);
 	});
 })()
 ;
