3a7957941bb1ed3081aa70541a0e0acb891a74f6
Valeria <vt@div-art.com>
2017-08-15 14:09:46 +0300

    ADDD

--- assets/js/controllers/StepFourController.js
+++ assets/js/controllers/StepFourController.js
@@ -38,12 +38,14 @@
 	    	$scope.element = [];
 	    	$scope.zacisk = [];
 	    	$scope.legars = [];
-	    	var h, h1, c, d, allHH, allHV, allLegarH, allLegarV;
+	    	var h, hV, h1,h11, c, d, allHH, allHV, allLegarH, allLegarV;
 	    	var h2 = 0;
+	    	var h22 = 0;
 
 	    	$scope.side.forEach( function(item, m, arr) {
 			  	if (item == 0)
 			  	{
+			  		h = 0;
 			  		allHH = 0;
 			  		allLegarH = 0;
 			  		$scope.terrace.y.forEach(function(l, j) {
@@ -51,13 +53,14 @@
 
 					  	allLegarH1 =  Math.ceil((($scope.terrace.y[j] * 1000) / 500) * (($scope.terrace.x[j] * 1000) / 2400));
 					  	
-				  		if (obj.paramSecondBoardY && ($scope.variants[m].twoBoards == true  || $scope.variants[m].twoBoards == true))
+				  		if (obj.paramSecondBoardY && $scope.variants[m].twoBoards == true )
 				    	{
 				    		h2 = (($scope.terrace.y[j] * 1000) / obj.paramSecondBoardY) * (obj.paramSecondBoardY / 500) * 2;
 				    	}
 
-				    	h = h1 + h2;
-				    	allHH =  Math.ceil(allHH + h);
+				    	h = h + h1 + h2;
+				    	console.log(h)
+				    	allHH =  Math.ceil(h);
 				    	allLegarH = allLegarH + allLegarH1;
 					});
 					
@@ -69,20 +72,20 @@
 			  	}
 			  	else
 			  	{
+			  		hV = 0;
 			  		allHV = 0;
 			  		allLegarV = 0;
 			  		$scope.terrace.x.forEach(function(l, j) {
-					  	h1 = (($scope.terrace.x[j] * 1000) / obj.paramFirstBoardY) * (obj.paramFirstBoardY / 500) * 2;
+					  	h11 = (($scope.terrace.x[j] * 1000) / obj.paramFirstBoardY) * (obj.paramFirstBoardY / 500) * 2;
 
 					  	allLegarV1 = Math.ceil((($scope.terrace.x[j] * 1000) / 500) * (($scope.terrace.y[j] * 1000) / 2400));
 
-				  		if (obj.paramSecondBoardY && ($scope.variants[m].twoBoards == true  || $scope.variants[m].twoBoards == true))
+				  		if (obj.paramSecondBoardY && $scope.variants[m].twoBoards == true)
 				    	{
-				    		h2 = (($scope.terrace.x[j] * 1000) / obj.paramSecondBoardY) * (obj.paramSecondBoardY / 500) * 2;
+				    		h22 = (($scope.terrace.x[j] * 1000) / obj.paramSecondBoardY) * (obj.paramSecondBoardY / 500) * 2;
 				    	}
-
-				    	h = h1 + h2;
-				    	allHV =  Math.ceil(allHV + h);
+				    	hV = hV + h11 + h22;
+				    	allHV =  Math.ceil(hV);
 				    	allLegarV = allLegarV + allLegarV1;
 					});
 					c = obj.zaciskPrice * allHV;
