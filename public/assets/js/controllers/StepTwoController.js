e1877a90e7abb9723e5d89f52f744328bcc6d3e0
DELL\Admin <vt@div-art.com>
2017-11-02 14:14:46 +0200

    add fix

--- public/assets/js/controllers/StepTwoController.js
+++ public/assets/js/controllers/StepTwoController.js
@@ -6,34 +6,6 @@
 			$scope.typeDeska($scope.deska);
 		});
 
-		$scope.typeDeska = function(type) { //выбор между композитной и деревенной первый шаг
-			$scope.deska = type;
-			$scope.cardsList = [];
-
-			if ($scope.deska == 'wooden')
-			{
-				$scope.cards.filter(function(item, i, arr) {
-
-					if (item.type_board == 'wooden')
-					{
-						$scope.cardsList.push(item);
-					}
-				});
-			}
-			if ($scope.deska == 'composite')
-			{
-				$scope.cards.filter(function(item, i, arr) {
-
-					if (item.type_board == 'composite')
-					{
-						$scope.cardsList.push(item);
-					}
-				});
-			}
-		};
-
-
-
 		/* C A R U S E L */
 		$scope.caruselClass = [];
 		$scope.positionItems = {};
